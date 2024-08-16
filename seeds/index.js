dde56b14f87c8cc50c9d225b930d570aa9fb3156
dariabilash <daribilash@gmail.com>
2024-08-16 15:00:45 -0500

    seed data of 50 locations

---
+++ seeds/index.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const Campground = require('../models/campground');
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/campio');
+    console.log('mongo is connected');
+}
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for(let i = 0; i < 50; i++) {
+        const rand1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[rand1000].city}, ${cities[rand1000].state}`
+        })
+        await camp.save();
+    }
+}
+
+seedDB();
