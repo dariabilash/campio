9c510f9028f065ec050228ff75c78106d8a7c8de
dariabilash <daribilash@gmail.com>
2024-08-16 15:14:36 -0500

    seed data of locations with places

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
+const {descriptors, places} = require('./seedHelpers');// destruction
 const Campground = require('../models/campground');
 
 main().catch(err => console.log(err));
@@ -9,15 +10,20 @@ async function main() {
     console.log('mongo is connected');
 }
 
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
 const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++) {
         const rand1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
-            location: `${cities[rand1000].city}, ${cities[rand1000].state}`
+            location: `${cities[rand1000].city}, ${cities[rand1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
         })
         await camp.save();
     }
 }
 
-seedDB();
+seedDB().then(() => {
+    mongoose.connection.close();
+})
